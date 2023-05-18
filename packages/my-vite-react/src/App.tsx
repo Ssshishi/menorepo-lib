6adacad04b55dd3832e65066e814d0a88fbcc91b
shenshi <shenshi@shouqianba.com>
2023-05-18 15:17:15 +0800

    feat: 11111

--- packages/my-vite-react/src/App.tsx
+++ packages/my-vite-react/src/App.tsx
@@ -3,7 +3,7 @@ import React, { useState } from 'react';
 const App: React.FC = () => {
   const [count, setCount] = useState(0);
 
-  const a = ' 12';
+  const a = '1221 ';
 
   return <>{a}</>;
 };
